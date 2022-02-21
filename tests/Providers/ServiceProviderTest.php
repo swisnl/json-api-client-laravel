b240e42750ef36d2b6360f7a516bf25b472cd62c
Jasper Zonneveld <jasper@swis.nl>
2022-02-21 13:54:34 +0100

    Add Laravel HTTP Client macros to consume JSON:API documents

---
+++ tests/Providers/ServiceProviderTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Swis\JsonApi\Client\Tests\Providers;
+
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use Swis\JsonApi\Client\Document;
+use Swis\JsonApi\Client\Tests\AbstractTest;
+
+class ServiceProviderTest extends AbstractTest
+{
+    protected function setUp(): void
+    {
+        if (!class_exists(Http::class)) {
+            $this->markTestSkipped('The Laravel HTTP Client is not available.');
+        }
+
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     */
+    public function itRegistersHttpFacadeMacroForRequest(): void
+    {
+        // arrange
+        Http::fake();
+
+        // act
+        Http::asJsonApi()->get('https://example.com');
+
+        // assert
+        Http::assertSent(static function (Request $request) {
+            return $request->hasHeader('Content-Type', 'application/vnd.api+json') &&
+                $request->hasHeader('Accept', 'application/vnd.api+json');
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function itRegistersHttpFacadeMacroForResponse(): void
+    {
+        // arrange
+        Http::fake();
+
+        // act
+        $data = Http::get('https://example.com')->jsonApi();
+
+        // assert
+        $this->assertInstanceOf(Document::class, $data);
+    }
+}
